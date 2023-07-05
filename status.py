db605af00a284a3a6832cb163ddc8d6645c9800c
zvz23 <ziegfredzorrilla23@gmail.com>
2023-07-05 13:34:03 +0800

    fix status

--- status.py
+++ status.py
@@ -3,5 +3,5 @@ from db import AllFreeNovelsDB
 with AllFreeNovelsDB('allfreenovels.db') as conn:
     total_books = conn.count_all_books()
     total_books_with_infos = conn.count_all_books_with_infos()
-    print("TOTAL BOOKS ", total_books_with_infos)
+    print("TOTAL BOOKS ", total_books)
     print("TOTAL WITH INFOS ", total_books_with_infos)
